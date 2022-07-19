5634fcf916c8d00c6cd01634b0e5b059f3fe27d6
clintonjosephs <clintonjosephs26@gmail.com>
2022-07-19 22:31:39 +0100

    introduced errors with context apis

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,7 @@ import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
  
-  const addTodoHandler = ( todoText: string ) => {
-    const newTodo = new Todo(todoText);
-    setTodos(prevTodos => [...prevTodos, newTodo]);
-  };
-
-  const removeTodoHandler = ( id: string ) => {
-    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
-  }
-
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler}/>
---
+++ src/store/todos-context.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Todo from '../models/todo';
+
+type TodosContextObj = {
+    items: Todo[];
+    addTodo: (text: string) => void;
+    removeTodo: (id: string) => void;
+  }
+
+export const TodosContext = React.createContext<TodosContextObj>({
+  items: [],
+  addTodo: (text: string) => {},
+  removeTodo: (id: string) => {},
+});
+
+const TodosContextProvider: React.FC = (props) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+
+  const removeTodoHandler = (id: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const contextValue: TodosContextObj = {
+    items: todos,
+    addTodo: addTodoHandler,
+    removeTodo: removeTodoHandler,
+  }
+
+  return <TodosContext.Provider value={contextValue}>{props.children}</TodosContext.Provider>;
+};
+
+export default TodosContextProvider;
